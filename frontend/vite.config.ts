c26e40f8f02902775e2806c3aa910292c5e48ba5
Vinod Akshat <pumkininriver@gmail.com>
2026-08-31 19:09:26 +0530

    chore: deployement

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,11 +8,11 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://warrant-1mia.onrender.com",
         changeOrigin: true,
       },
       "/health": {
-        target: "http://localhost:3000",
+        target: "https://warrant-1mia.onrender.com",
         changeOrigin: true,
       },
     },
